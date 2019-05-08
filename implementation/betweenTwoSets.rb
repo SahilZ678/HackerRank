53d765f2ab6d1410bed337586640659e4d5c6c28
Sahil Maharjan <sahil.maharjan678@gmail.com>
2019-05-08 14:23:35 +0545

    corrected BetweenTwoSets

--- implementation/betweenTwoSets.rb
+++ implementation/betweenTwoSets.rb
@@ -1,8 +1,18 @@
 # https://www.hackerrank.com/challenges/between-two-sets/problem
 
 def getTotalX(a,b)
-lcm = a.reduce(1,:lcm)
-print lcm
+lcm = a.reduce(:lcm)
+gcd = b.reduce(:gcd)
+count = 0
+
+# print (lcm..gcd).to_a
+(lcm..gcd).step(lcm) do |i|
+    puts i
+    if gcd%i == 0
+        count += 1
+    end
+end
+count
 end
 nm = gets.rstrip.split
 n = nm[0].to_i
@@ -10,3 +20,4 @@ m = nm[1].to_i
 a = gets.rstrip.split(' ').map(&:to_i)
 b = gets.rstrip.split(' ').map(&:to_i)
 total = getTotalX a, b
+puts total
